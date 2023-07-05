2b9d54f73b235c516f09c3dc399183b023ffa3cb
Abhay Vishwakarma <abhay.vishwakarma@neosoftmail.com>
2023-07-05 14:14:17 +0530

    BlogStack Master Service Project Initiated

---
+++ src/main/java/com/blogstack/BlogStackMasterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStackMasterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogStackMasterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogstack/BlogStackMasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogStackMasterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
